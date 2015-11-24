cf94501fbc9c5b213a826523eb96b8cfec00b873
gaw3 <gwhite2003@verizon.net>
2015-11-23 18:41:19 -0600

    Create Xcode project

---
+++ OnTheMap/OnTheMap/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  OnTheMap
+//
+//  Created by Gregory White on 11/23/15.
+//  Copyright © 2015 Gregory White. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
