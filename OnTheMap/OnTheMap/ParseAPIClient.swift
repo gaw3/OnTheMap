398acde823665903af8c75aa1e5a5642012d0128
gaw3 <gwhite2003@verizon.net>
2015-11-30 18:53:13 -0600

    Add Parse API - GET Student Locations & POST Student Location

--- OnTheMap/OnTheMap/ParseAPIClient.swift
+++ OnTheMap/OnTheMap/ParseAPIClient.swift
@@ -1,38 +1,30 @@
 //
-//  UdacityAPIClient.swift
+//  ParseAPIClient.swift
 //  OnTheMap
 //
-//  Created by Gregory White on 11/24/15.
+//  Created by Gregory White on 11/30/15.
 //  Copyright © 2015 Gregory White. All rights reserved.
 //
 
 import UIKit
 
-typealias LoginCompletionHandler = (result: AnyObject!, error: NSError?) -> Void
+typealias ParseAPICompletionHandler = (result: AnyObject!, error: NSError?) -> Void
 
-class UdacityAPIClient: NSObject {
+class ParseAPIClient: NSObject {
 
-	class func logout(completionHandler: LoginCompletionHandler) {
-		let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
-		request.HTTPMethod = "DELETE"
-
-		let cookies: [NSHTTPCookie] = NSHTTPCookieStorage.sharedHTTPCookieStorage().cookies!
-		
-		let index = cookies.indexOf { (cookie) -> Bool in
-			return (cookie.name == "XSRF-TOKEN")
-		}
-
-		if let index = index {
-			request.setValue(cookies[index].value, forHTTPHeaderField: "X-XSRF-TOKEN")
-		}
+	class func getStudentLocations(completionHandler: ParseAPICompletionHandler) {
+		let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation?limit=5")!)
+		request.HTTPMethod = "GET"
+		request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+		request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
 
 		let session = NSURLSession.sharedSession()
 		let task    = session.dataTaskWithRequest(request) { (rawJSONResponse, httpResponse, urlSessionError) in
 
 			guard urlSessionError == nil else {
 				let userInfo: [NSObject : AnyObject] = [NSLocalizedDescriptionKey : "Network Error",
-																	 NSUnderlyingErrorKey      : urlSessionError!]
-				let error = NSError(domain: "Udacity API Logout", code: 1, userInfo: userInfo)
+					                                     NSUnderlyingErrorKey      : urlSessionError!]
+				let error = NSError(domain: "Parse API Get Student Locations", code: 1, userInfo: userInfo)
 
 				completionHandler(result: nil, error: error)
 				return
@@ -46,7 +38,7 @@ class UdacityAPIClient: NSObject {
 
 				let userInfo: [NSObject : AnyObject] = [NSLocalizedDescriptionKey        : "HTTP Error",
 																	 NSLocalizedFailureReasonErrorKey : failureReason]
-				let error = NSError(domain: "Udacity API Logout", code: 2, userInfo: userInfo)
+				let error = NSError(domain: "Parse API Get Student Locations", code: 2, userInfo: userInfo)
 
 				completionHandler(result: nil, error: error)
 				return
@@ -55,86 +47,20 @@ class UdacityAPIClient: NSObject {
 			guard let rawJSONResponse = rawJSONResponse else {
 				let userInfo: [NSObject : AnyObject] = [NSLocalizedDescriptionKey        : "JSON Error",
 																	 NSLocalizedFailureReasonErrorKey : "Could not find raw JSON data"]
-				let error = NSError(domain: "Udacity API Logout", code: 3, userInfo: userInfo)
+				let error = NSError(domain: "Parse API Get Student Locations", code: 3, userInfo: userInfo)
 
 				completionHandler(result: nil, error: error)
 				return
 			}
-
-			let actualJSONData = rawJSONResponse.subdataWithRange(NSMakeRange(5, rawJSONResponse.length - 5))
 
 			do {
-				let parsedJSONResponse = try NSJSONSerialization.JSONObjectWithData(actualJSONData, options: .AllowFragments) as! Dictionary<String, AnyObject>
-
-				completionHandler(result: parsedJSONResponse, error: nil)
-			} catch let jsonError as NSError {
-				let userInfo: [NSObject : AnyObject] = [NSLocalizedDescriptionKey : "JSON Serialization Error",
-																    NSUnderlyingErrorKey      : jsonError]
-				let error = NSError(domain: "Udacity API Logout", code: 4, userInfo: userInfo)
-
-				completionHandler(result: nil, error: error)
-				return
-			}
-
-		}
-
-		task.resume()
-	}
-
-	class func loginWithUsername(username: String, password: String, completionHandler: LoginCompletionHandler) {
-		let request        = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
-		let udacityJSONObj = [ "udacity": [ "username": username, "password": password ] ]
-
-		request.HTTPMethod = "POST"
-		request.addValue("application/json", forHTTPHeaderField: "Accept")
-		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-		request.HTTPBody = try! NSJSONSerialization.dataWithJSONObject(udacityJSONObj, options: .PrettyPrinted)
-
-      let session = NSURLSession.sharedSession()
-		let task    = session.dataTaskWithRequest(request) { (rawJSONResponse, httpResponse, urlSessionError) in
-
-			guard urlSessionError == nil else {
-				let userInfo: [NSObject : AnyObject] = [NSLocalizedDescriptionKey : "Network Error",
-																	 NSUnderlyingErrorKey      : urlSessionError!]
-				let error = NSError(domain: "Udacity API Login", code: 1, userInfo: userInfo)
-
-				completionHandler(result: nil, error: error)
-				return
-			}
-
-			let httpURLResponse = httpResponse as? NSHTTPURLResponse
-
-			guard httpURLResponse?.statusCodeClass == .Successful else {
-				let httpStatusText = NSHTTPURLResponse.localizedStringForStatusCode((httpURLResponse?.statusCode)!)
-				let failureReason  = "HTTP status code = \(httpURLResponse?.statusCode), HTTP status text = \(httpStatusText)"
-
-				let userInfo: [NSObject : AnyObject] = [NSLocalizedDescriptionKey        : "HTTP Error",
-																	 NSLocalizedFailureReasonErrorKey : failureReason]
-				let error = NSError(domain: "Udacity API Login", code: 2, userInfo: userInfo)
-
-				completionHandler(result: nil, error: error)
-				return
-			}
-
-			guard let rawJSONResponse = rawJSONResponse else {
-				let userInfo: [NSObject : AnyObject] = [NSLocalizedDescriptionKey        : "JSON Error",
-																	 NSLocalizedFailureReasonErrorKey : "Could not find raw JSON data"]
-				let error = NSError(domain: "Udacity API Login", code: 3, userInfo: userInfo)
-
-				completionHandler(result: nil, error: error)
-				return
-			}
-
-			let actualJSONData = rawJSONResponse.subdataWithRange(NSMakeRange(5, rawJSONResponse.length - 5))
-
-			do {
-				let parsedJSONResponse = try NSJSONSerialization.JSONObjectWithData(actualJSONData, options: .AllowFragments) as! Dictionary<String, AnyObject>
+				let parsedJSONResponse = try NSJSONSerialization.JSONObjectWithData(rawJSONResponse, options: .AllowFragments) as! Dictionary<String, AnyObject>
 
 				completionHandler(result: parsedJSONResponse, error: nil)
 			} catch let jsonError as NSError {
 				let userInfo: [NSObject : AnyObject] = [NSLocalizedDescriptionKey : "JSON Serialization Error",
 																	 NSUnderlyingErrorKey      : jsonError]
-				let error = NSError(domain: "Udacity API Login", code: 4, userInfo: userInfo)
+				let error = NSError(domain: "Parse API Get Student Locations", code: 4, userInfo: userInfo)
 
 				completionHandler(result: nil, error: error)
 				return
@@ -145,5 +71,75 @@ class UdacityAPIClient: NSObject {
 		task.resume()
 	}
 
-}
+	class func postStudentLocation(completionHandler: ParseAPICompletionHandler) {
+		let student = [ "uniqueKey" : "blap",
+			"firstName" : "Jonathan",
+			"lastName" : "Hemlock",
+			"mapString" : "Dallas, TX",
+			"mediaURL" : "https://udacity.com",
+			"latitude" : 32.7767,
+			"longitude" : 96.797]
 
+		let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation")!)
+
+		request.HTTPMethod = "POST"
+		request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+		request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+      request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+		request.HTTPBody = try! NSJSONSerialization.dataWithJSONObject(student, options: .PrettyPrinted)
+
+		let session = NSURLSession.sharedSession()
+		let task    = session.dataTaskWithRequest(request) { (rawJSONResponse, httpResponse, urlSessionError) in
+
+			guard urlSessionError == nil else {
+				let userInfo: [NSObject : AnyObject] = [NSLocalizedDescriptionKey : "Network Error",
+					NSUnderlyingErrorKey      : urlSessionError!]
+				let error = NSError(domain: "Parse API Get Student Locations", code: 1, userInfo: userInfo)
+
+				completionHandler(result: nil, error: error)
+				return
+			}
+
+			let httpURLResponse = httpResponse as? NSHTTPURLResponse
+
+			guard httpURLResponse?.statusCodeClass == .Successful else {
+				let httpStatusText = NSHTTPURLResponse.localizedStringForStatusCode((httpURLResponse?.statusCode)!)
+				let failureReason  = "HTTP status code = \(httpURLResponse?.statusCode), HTTP status text = \(httpStatusText)"
+
+				let userInfo: [NSObject : AnyObject] = [NSLocalizedDescriptionKey        : "HTTP Error",
+					NSLocalizedFailureReasonErrorKey : failureReason]
+				let error = NSError(domain: "Parse API Get Student Locations", code: 2, userInfo: userInfo)
+
+				completionHandler(result: nil, error: error)
+				return
+			}
+
+			guard let rawJSONResponse = rawJSONResponse else {
+				let userInfo: [NSObject : AnyObject] = [NSLocalizedDescriptionKey        : "JSON Error",
+					NSLocalizedFailureReasonErrorKey : "Could not find raw JSON data"]
+				let error = NSError(domain: "Parse API Get Student Locations", code: 3, userInfo: userInfo)
+
+				completionHandler(result: nil, error: error)
+				return
+			}
+
+			do {
+				let parsedJSONResponse = try NSJSONSerialization.JSONObjectWithData(rawJSONResponse, options: .AllowFragments) as! Dictionary<String, AnyObject>
+
+				completionHandler(result: parsedJSONResponse, error: nil)
+			} catch let jsonError as NSError {
+				let userInfo: [NSObject : AnyObject] = [NSLocalizedDescriptionKey : "JSON Serialization Error",
+					NSUnderlyingErrorKey      : jsonError]
+				let error = NSError(domain: "Parse API Get Student Locations", code: 4, userInfo: userInfo)
+
+				completionHandler(result: nil, error: error)
+				return
+			}
+
+		}
+
+		task.resume()
+	}
+
+
+}
--- OnTheMap/OnTheMap/UdacityLoginViewController.swift
+++ OnTheMap/OnTheMap/UdacityLoginViewController.swift
@@ -19,13 +19,12 @@ class UdacityLoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 
    // MARK: - IB Actions
 
 	@IBAction func loginButtonTouchUpInside(sender: UIButton) {
-		UdacityAPIClient.loginWithUsername("gwhite2003@verizon.net", password: "spanky", completionHandler: completeLogin)
+		UdacityAPIClient.loginWithUsername("gwhite2003@verizon.net", password: "chopper", completionHandler: completeLogin)
 		// 1. verify textfields have something in them
 		// 2. use udacity client to login
 		// 2.5 if logged in, move to main tabbar controller
@@ -40,6 +39,51 @@ class UdacityLoginViewController: UIViewController {
 		// 3. provide error indicators to user
 	}
 
+
+	@IBAction func getStudentLocationsButtonDidTouchUpInside(sender: UIButton) {
+		ParseAPIClient.getStudentLocations(completeGetStudentLocations)
+	}
+
+	@IBAction func postStudentLocationButtonDidTouchUp(sender: UIButton) {
+		ParseAPIClient.postStudentLocation(completePostStudentLocation)
+	}
+	
+	func completeGetStudentLocations(result: AnyObject!, error: NSError?) {
+
+		guard error == nil else {
+			// handle error
+			return
+		}
+
+		guard result != nil else {
+			// handle error
+			return
+		}
+
+		let json = result as! Dictionary<String, AnyObject>
+
+		if let studentLocations = json["results"] {
+			print("student locations json = \(studentLocations)")
+		}
+
+	}
+
+	func completePostStudentLocation(result: AnyObject!, error: NSError?) {
+
+		guard error == nil else {
+			// handle error
+			return
+		}
+
+		guard result != nil else {
+			// handle error
+			return
+		}
+
+		let json = result as! Dictionary<String, AnyObject>
+		print("post student location json = \(json)")
+	}
+	
 	func completeLogin(result: AnyObject!, error: NSError?) {
 
 		guard error == nil else {
@@ -47,12 +91,13 @@ class UdacityLoginViewController: UIViewController {
 			return
 		}
 
-		guard result == nil else {
+		guard result != nil else {
 			// handle error
 			return
 		}
 
 		let json = result as! Dictionary<String, AnyObject>
+		print("login json = \(json)")
 
 		if let jsonSession = json["session"] {
 
@@ -77,12 +122,13 @@ class UdacityLoginViewController: UIViewController {
 			return
 		}
 
-		guard result == nil else {
+		guard result != nil else {
 			// handle error
 			return
 		}
 
 		let json = result as! Dictionary<String, AnyObject>
+		print("logout json = \(json)")
 
 		if let jsonSession = json["session"] {
 
