7891502d140ada4d47349badeb31fb2d641cb22e
gaw3 <gwhite2003@verizon.net>
2020-03-13 14:58:11 -0500

    * Create Udacity Blue * Restart the entire storyboard * Remove SwiftyBeaver

---
+++ OnTheMap/OnTheMap/LoginController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginController.swift
+//  OnTheMap
+//
+//  Created by Gregory White on 3/13/20.
+//  Copyright © 2020 Gregory White. All rights reserved.
+//
+
+import UIKit
+
+//import SwiftyBeaver
+
+class LoginController: UIViewController {
+    
+    @IBOutlet weak var emailField:    UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton:   UIButton!
+    @IBOutlet weak var signUpButton:  UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
--- OnTheMap/Pods/Target Support Files/Pods-OnTheMap/Pods-OnTheMap-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OnTheMap : NSObject
-@end
-@implementation PodsDummy_Pods_OnTheMap
-@end
