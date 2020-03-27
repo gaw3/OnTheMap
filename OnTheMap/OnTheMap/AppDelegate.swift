6b6932412e65ddede1610d74864b00afc8126f89
gaw3 <gwhite2003@verizon.net>
2020-03-26 19:20:24 -0500

    Remove the FB stuff

--- OnTheMap/OnTheMap/AppDelegate.swift
+++ OnTheMap/OnTheMap/AppDelegate.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 
-//import FBSDKCoreKit
-
 let dataMgr = DataManager.shared
 
 @UIApplicationMain
@@ -18,13 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
     }
-    
-//    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-//        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
-//    }
     
 }
 
